5c249944ad5e54699568aa4a15743ee3909fe627
luljaj <lukauljaj43@gmail.com>
2026-01-16 15:18:43 -0500

    Changed local config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@supabase/ssr", "@supabase/supabase-js"],
 };
 
 module.exports = nextConfig;
